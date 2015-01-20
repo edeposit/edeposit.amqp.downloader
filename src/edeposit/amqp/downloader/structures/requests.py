42f665ae073db95f8c68a5e2dfd573dac383faf8
Bystroushaak <bystrousak@kitakitsune.org>
2015-01-20 16:05:10 +0100

    Added sketch of the communication structures.

---
+++ src/edeposit/amqp/downloader/structures/requests.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+
+
+
+# Variables ===================================================================
+
+
+
+# Functions & classes =========================================================
+class Download(url):
+    pass
+
+
+class DownloadWithProgress(url):
+    pass
+
+
+class CheckExistence(url):
+    pass
---
+++ src/edeposit/amqp/downloader/structures/responses.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+
+
+
+# Variables ===================================================================
+
+
+
+# Functions & classes =========================================================
+class DownloadedFile(url, base64_data):
+    pass
+
+
+class Progress(url, progress):
+    pass
+
+
+class Exists(url, result):
+    pass
