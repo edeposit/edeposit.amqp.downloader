f01f6bd88a71436cf7164549ecee79a6e36f904f
Bystroushaak <bystrousak@kitakitsune.org>
2015-02-04 16:43:16 +0100

    #4: Added docstrings to request structures.

--- src/edeposit/amqp/downloader/structures/requests.py
+++ src/edeposit/amqp/downloader/structures/requests.py
@@ -7,15 +7,44 @@
 from collections import namedtuple
 
 
-# Variables ===================================================================
 # Functions & classes =========================================================
 class Download(namedtuple("Download", ["url"])):
+    """
+    Download data from `url`.
+
+    Attributes:
+        url (str): URL of the internet resource.
+
+    Returns:
+        obj: :class:`.DownloadedFile`.
+    """
     pass
 
 
 class ProgressDownload(namedtuple("ProgressDownload", ["url", "steps"])):
+    """
+    Download data from `url` and report back the progress.
+
+    Attributes:
+        url (str): URL of the internet resource.
+        steps (int): Number of steps used to track progress.
+
+    Progress is reported using :class:`.Progress` structure.
+
+    Returns:
+        obj: :class:`.DownloadedFile`.
+    """
     pass
 
 
 class CheckExistence(namedtuple("CheckExistence", ["url"])):
+    """
+    Send HEAD request to given `url` and check it's existence.
+
+    Attributes:
+        url (str): URL of the internet resource.
+
+    Returns:
+        obj: :class:`.Exists`.
+    """
     pass
